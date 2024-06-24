e8b8a081925a15f8db6d25494b766be965a21e4c
Vasilii_Komarov <vasilii_komarov@epam.com>
2024-06-24 19:13:36 +0200

    Basic project structure

---
+++ cadabra-shop-impl/src/main/java/com/epam/komarov/cadabra/shop/CadabraShopApplication.java
@@ -0,0 +1,13 @@
+package com.epam.komarov.cadabra.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadabraShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadabraShopApplication.class, args);
+    }
+
+}
